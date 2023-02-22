3b24af4df1233b37b60971b193c6367535b992e3
Enrique <enrique.barbosasilva@gmail.com>
2023-02-22 12:40:21 -0300

    feat: listing vehicle routes

--- src/routes/vehicle.routes.ts
+++ src/routes/vehicle.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import createUserController from "../controllers/users/createUser.controller";
 import createVehicleController from "../controllers/vehicle/CreateVehicle.controller";
+import listVehiclesController from "../controllers/vehicle/ListVehicle.controller";
+import listVehicleByIdController from "../controllers/vehicle/ListVehicleById.controller";
 import getAuthMiddleware from "../middlewares/getAuth.middleware";
 
 const vehicleRoutes = Router();
 
 vehicleRoutes.post("", getAuthMiddleware, createVehicleController);
+vehicleRoutes.get("", listVehiclesController);
+vehicleRoutes.get("/:id", listVehicleByIdController);
 
 export default vehicleRoutes;
