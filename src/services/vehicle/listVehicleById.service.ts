85d431f6d37bfea5607cb2b171f1b00499081065
Enrique <enrique.barbosasilva@gmail.com>
2023-02-22 12:42:37 -0300

    feat: list vehicleById service

---
+++ src/services/vehicle/listVehicleById.service.ts
@@ -0,0 +1,17 @@
+import { AppDataSource } from "../../data-source";
+import { Vehicle } from "../../entities/vehicle.entity";
+import AppError from "../../errors/appError";
+
+const listVehicleByIdService = async (id: string): Promise<Vehicle> => {
+  const vehicleRepository = AppDataSource.getRepository(Vehicle);
+
+  const vehicle = await vehicleRepository.findOneBy({ id });
+
+  if (!vehicle) {
+    throw new AppError("Vehicle not found", 404);
+  }
+
+  return vehicle;
+};
+
+export default listVehicleByIdService;
