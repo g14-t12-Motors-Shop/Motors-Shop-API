f899b319413c3779218fcbe11e4150a5deace373
=Welton Santos <weltonsantosfr@gmail.com>
2023-02-23 13:39:45 -0300

    hotfix: fix cors in app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,10 @@ import vehicleRoutes from "./routes/vehicle.routes";
 const app = express();
 app.use(express.json());
 
+app.use(cors());
 app.use("/login", sessionRoutes);
 app.use("/users", userRoutes);
 app.use("/vehicles", vehicleRoutes);
-
-app.use(cors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
