e259f65de57d49bc9f698da76ade4b4454741d48
=Welton Santos <weltonsantosfr@gmail.com>
2023-02-28 18:34:00 -0300

    hotfix: updateUserController

--- src/controllers/users/updateUser.controller.ts
+++ src/controllers/users/updateUser.controller.ts
@@ -3,10 +3,10 @@ import { Request, Response } from "express";
 import updateUSerService from "../../services/users/updateUser.service";
 
 const updateUserController = async (req: Request, res: Response) => {
-    const user = req.body;
-    const id: string = req.params.id;
-    const updatedUser = await updateUSerService(user, id);
-    return res.json(instanceToPlain(updatedUser));
-}
+  const user = req.body;
+  const id: string = req.user.id;
+  const updatedUser = await updateUSerService(user, id);
+  return res.json(instanceToPlain(updatedUser));
+};
 
 export default updateUserController;
