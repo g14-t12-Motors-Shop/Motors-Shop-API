68a6a394227b9724148ef4beb0b7df907ead1b5e
Enrique <enrique.barbosasilva@gmail.com>
2023-03-06 23:40:28 -0300

    refactor: implemented owner in listing vehicle

--- src/services/vehicle/listVehicleById.service.ts
+++ src/services/vehicle/listVehicleById.service.ts
@@ -5,7 +5,16 @@ import AppError from "../../errors/appError";
 const listVehicleByIdService = async (id: string): Promise<Vehicle> => {
   const vehicleRepository = AppDataSource.getRepository(Vehicle);
 
-  const vehicle = await vehicleRepository.findOneBy({ id });
+  const vehicle = await vehicleRepository.findOne({
+    where: {
+      id,
+    },
+    relations: {
+      owner: {
+        vehicle: true,
+      },
+    },
+  });
 
   if (!vehicle) {
     throw new AppError("Vehicle not found", 404);
